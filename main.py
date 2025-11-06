c114e9abcf0d2c1cf8401b379d0a82eedf0ea28f
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-11-06 13:56:40 +0100

    extract constants

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from pathlib import Path
 
 from src.utils.registry import SOLUTIONS
 
+EXPECTED_ARGV_LEN = 4
+ERR_BAD_ARG = r"want: year[\d+] day[\d+] part[abc]"
+
 
 def get_task(year: str, day: str, part: str) -> str:
     path = Path() / "notes" / f"year{year}" / f"day{day}{part}.txt"
@@ -11,7 +14,7 @@ def get_task(year: str, day: str, part: str) -> str:
 
 
 def main() -> None:
-    assert len(sys.argv) == 4, r"want: year[\d+] day[\d+] part[abc]"
+    assert len(sys.argv) == EXPECTED_ARGV_LEN, ERR_BAD_ARG
 
     year, day, part = sys.argv[1:]
     key = f"{year}-{day}{part}"

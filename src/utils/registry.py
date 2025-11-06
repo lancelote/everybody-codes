fa5261410abd9f74700a20907d637b5789ce4493
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-11-06 14:01:18 +0100

    fix solution name assertion

--- src/utils/registry.py
+++ src/utils/registry.py
@@ -8,7 +8,7 @@ SOLUTIONS: dict[str, Solution] = {}
 
 def register_solution(name: str) -> Callable[[Solution], Solution]:
     # e.g., `2025-1a`
-    assert valid_solution_name(name), f"invalid solution name={name}"
+    assert not is_valid_solution_name(name), f"invalid solution name={name}"
 
     def decorator(func: Solution) -> Solution:
         assert name not in SOLUTIONS
@@ -19,5 +19,5 @@ def register_solution(name: str) -> Callable[[Solution], Solution]:
     return decorator
 
 
-def valid_solution_name(name: str) -> bool:
+def is_valid_solution_name(name: str) -> bool:
     return re.match(r"\d{4}-\d{1,2}[abc]", name) is None
